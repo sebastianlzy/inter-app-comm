6690e408e14f305a2c4afa17d639d621269c592d
sebastian <sebastian@shopback.com>
2018-04-10 10:47:52 +0800

    print msg on html;

--- src/log.js
+++ src/log.js
@@ -4,6 +4,8 @@ const log = debug('IAC');
 
 export default {
   debug: (msg, ...args) => {
-    log(msg, ...args)
+    log(msg, ...args);
+    const responseHtml = document.getElementById('response');
+    responseHtml.insertAdjacentHTML('beforeend', `<div>${msg}</div>`);
   }
 }
